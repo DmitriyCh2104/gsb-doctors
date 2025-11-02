bc21407f18783734a638427739dabcc7dfaa2d5c
DmitriyCh2104 <dmitriy.chekunkov@gmail.com>
2025-11-02 06:35:46 +0100

    feat(components): add DoctorCardComponent and styles

---
+++ gsb-doctors/src/app/components/doctor-card/doctor-card.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Doctor } from '../../types/doctor.interface';
+
+@Component({
+  selector: 'app-doctor-card',
+  imports: [],
+  templateUrl: './doctor-card.html',
+  styleUrl: './doctor-card.css',
+})
+export class DoctorCard {
+  @Input() doctor!: Doctor;
+}
