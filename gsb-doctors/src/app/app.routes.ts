4e47b4b0515d1c5919b9e764d9f2a8219f1e5d59
DmitriyCh2104 <dmitriy.chekunkov@gmail.com>
2025-11-02 02:52:28 +0100

    feat(routes): add routes for doctors page and redirection

--- gsb-doctors/src/app/app.routes.ts
+++ gsb-doctors/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DoctorsPageComponent } from './pages/doctors-page/doctors-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/medecins', pathMatch: 'full' },  // Route racine redirige vers /medecins
+  { path: 'medecins', component: DoctorsPageComponent },    // Route pour afficher la liste des médecins
+  { path: '**', redirectTo: '/medecins' }                    // Route wildcard pour rediriger vers /medecins (page d'erreur à venir)
+];
