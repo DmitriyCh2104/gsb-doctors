405797ee41f724a7baccf717e722fa9a7a980267
DmitriyCh2104 <dmitriy.chekunkov@gmail.com>
2025-11-02 02:15:12 +0100

    chore(assets): add doctors.json and validate assets config

--- gsb-doctors/src/app/types/doctor.interface.ts
+++ gsb-doctors/src/app/types/doctor.interface.ts
@@ -1,8 +1,8 @@
 export interface Doctor {
-  id: number;  
-  firstName: string;  
-  lastName : string;
-  email : string;
-  speciality : string;
-  address : string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  speciality: string;
+  address: string;
 }
